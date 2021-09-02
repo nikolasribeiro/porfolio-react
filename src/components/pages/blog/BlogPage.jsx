e51921d4c58dd49ca6dc3b3fd78edc776b7126d1
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2021-09-02 10:37:48 -0300

    improve on porfolio page and blog page, loading component installed

--- src/components/pages/blog/BlogPage.jsx
+++ src/components/pages/blog/BlogPage.jsx
@@ -1,20 +1,34 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { PorfolioContext } from '../../Context/PorfolioContext';
 import { BlogPost } from './blogComponent/BlogPost';
+import ReactLoading from "react-loading";
 
 import './BlogPage.scss';
 
 export const BlogPage = () => {
 
     const { blogs, setToggleMenu } = useContext(PorfolioContext);
-
-    useEffect(() => {
-        setToggleMenu(false);
-    }, [setToggleMenu]);
-
     const [inputSearch, setInputSearch] = useState({
         search: ""
     });
+    const [isBlogsLoading, setIsBlogsLoading] = useState(true);
+
+    useEffect(() => {
+        setToggleMenu(false);
+        
+    }, [setToggleMenu]);
+
+    useEffect(()=>{
+        console.log('Esto es el largo de blogs: ', blogs.length);
+        if (blogs.length === 0){
+            console.log('Estan cargando los blogs!')
+            setIsBlogsLoading(true);
+        }else{
+            console.log('Ya se cargaron los blogs!');
+            setIsBlogsLoading(false);
+        }
+    }, [blogs]);
+
     const { search } = inputSearch;
 
     const handleInputChange = ({ target }) => {
@@ -29,33 +43,44 @@ export const BlogPage = () => {
     ));
 
     return (
-        <div className="blog-wrapper">
-
-            <div className="blog-container">
-
-                {filteredBlogs.reverse().map(post => (
-                    <BlogPost post={post} key={post.id}/>
-                ))}
-
+        <>
+        {isBlogsLoading 
+        ? 
+            <div className="loading-container">
+                <ReactLoading
+                    type="bars" 
+                    color="#F26C4F" 
+                    width={130}
+                />
             </div>
+        : (
+            <div className="blog-wrapper">
+                <div className="blog-container">
 
-            <aside className="blog-advanceInfo">
+                    {filteredBlogs.reverse().map(post => (
+                        <BlogPost post={post} key={post.id}/>
+                    ))}
 
-                <div className="searchbar-container">
-                    <input 
-                        type="text" 
-                        placeholder="Search"
-                        onChange={handleInputChange}
-                        name="search"
-                        value={search}
-                    />
-                    <button 
-                        type="submit"
-                    ><i className="fas fa-search"></i></button>
                 </div>
-                
-            </aside>
 
-        </div>
+                <aside className="blog-advanceInfo">
+
+                    <div className="searchbar-container">
+                        <input 
+                            type="text" 
+                            placeholder="Search"
+                            onChange={handleInputChange}
+                            name="search"
+                            value={search}
+                        />
+                        <button 
+                            type="submit"
+                        ><i className="fas fa-search"></i></button>
+                    </div>
+                    
+                </aside>
+            </div>
+        )}
+        </>
     );
 }
--- src/components/pages/porfolio/PorfolioPage.jsx
+++ src/components/pages/porfolio/PorfolioPage.jsx
@@ -1,18 +1,27 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { PorfolioContext } from '../../Context/PorfolioContext';
+import ReactLoading from "react-loading";
 import './PorfolioPage.scss';
 
 export const PorfolioPage = () => {
 
     const { uniqueTechnologies, projects, setToggleMenu } = useContext(PorfolioContext);
     const [filteredProjects, setFilteredProjects] = useState(projects);
+    const [isProjectsLoading, setIsProjectLoading] = useState(true);
 
     useEffect(() => {
         setToggleMenu(false);
     }, [setToggleMenu])
 
-    useEffect(()=>{}, [projects]);
+    useEffect(()=>{
+        if (projects.length === 0){
+            setIsProjectLoading(true);
+        }else{
+            setIsProjectLoading(false);
+            setFilteredProjects(projects);
+        }
+    }, [projects]);
 
     const handleClick = (technologie) => {
         
@@ -34,67 +43,79 @@ export const PorfolioPage = () => {
 
     return (
         <section className="grid-layout">
-            
-            <div className="filter-by-technologies">
+            {isProjectsLoading 
+            ? 
+                <div className="loading-container">
+                    <ReactLoading 
+                        type="bars" 
+                        color="#F26C4F" 
+                        width={130}
+                    />
+                </div>
+            : (
+            <>
+                <div className="filter-by-technologies">
 
-                <h4>Search By Technologies</h4>
-                <p onClick={() => handleClick('all')}>Show All Projects</p>
-                <ul className="list-tech">
-                    {uniqueTechnologies.map(tech => 
-                        <li key={tech} onClick={() => handleClick(`${tech}`)}>{tech}</li>
-                    )}
-                </ul>
+                    <h4>Search By Technologies</h4>
+                    <p onClick={() => handleClick('all')}>Show All Projects</p>
+                    <ul className="list-tech">
+                        {uniqueTechnologies.map(tech => 
+                            <li key={tech} onClick={() => handleClick(`${tech}`)}>{tech}</li>
+                        )}
+                    </ul>
 
-            </div>
+                </div>
 
-            <div className="list-projects">
-                {filteredProjects.map(project => (
+                <div className="list-projects">
+                    {filteredProjects.map(project => (
 
-                    <div key={project.number} className={`project-wrapper animate__animated animate__fadeInLeft animate__faster`}>
-                        
-                        <div className="project-number">
-                            <h4>{project.number}</h4>
-                        </div>
+                        <div key={project.number} className={`project-wrapper animate__animated animate__fadeInLeft animate__faster`}>
+                            
+                            <div className="project-number">
+                                <h4>{project.number}</h4>
+                            </div>
 
-                        <div className="project-header">
-                            <img 
-                                src={project.image_project}
-                                alt={project.name_project}
-                            />
-                        </div>
+                            <div className="project-header">
+                                <img 
+                                    src={project.image_project}
+                                    alt={project.name_project}
+                                />
+                            </div>
 
-                        <div className="project-container">
+                            <div className="project-container">
 
-                            <div className="project-body">
-                                <h2>{project.name_project}</h2>
-                                <p className="project-description">
-                                    {project.short_description}
-                                </p>
+                                <div className="project-body">
+                                    <h2>{project.name_project}</h2>
+                                    <p className="project-description">
+                                        {project.short_description}
+                                    </p>
 
-                                <div className="buttons">
-                                    <Link to={`./porfolio/${project.number}`}>Read More</Link>
-                                    <a 
-                                        href={project.source_code} 
-                                        target="_blank" 
-                                        rel="noreferrer"
-                                    >Source Code</a>
+                                    <div className="buttons">
+                                        <Link to={`./porfolio/${project.number}`}>Read More</Link>
+                                        <a 
+                                            href={project.source_code} 
+                                            target="_blank" 
+                                            rel="noreferrer"
+                                        >Source Code</a>
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div className="technologies">
-                                {   
-                                    project.technologies.split(', ').map(tech => (
-                                        <p key={tech}>{tech}</p>
-                                    ))
-                                }
+                                <div className="technologies">
+                                    {   
+                                        project.technologies.split(', ').map(tech => (
+                                            <p key={tech}>{tech}</p>
+                                        ))
+                                    }
+                                </div>
+
                             </div>
 
                         </div>
 
-                    </div>
-
-                ))}
-            </div>
+                    ))}
+                </div>
+            </>
+            )}
         </section>
 
     );
